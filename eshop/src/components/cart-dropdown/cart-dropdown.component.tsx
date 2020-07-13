e69e8db855f9bc6b7d79de50e054ef908f21fd0b
nishank bangera <nishank.net@gmail.com>
2020-07-14 00:29:40 +0530

    adding items to cart and displaying in dropdown

--- eshop/src/components/cart-dropdown/cart-dropdown.component.tsx
+++ eshop/src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import './cart-dropdown.styles.scss'
-import { CustomButton } from '../common/controls/button/button.component'
-
+import React from "react";
+import "./cart-dropdown.styles.scss";
+import { CustomButton } from "../common/controls/button/button.component";
+import { useSelector } from "react-redux";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
-    return (
-        <div className="cart-dropdown">
-            <div className="cart-items"></div>
-            <CustomButton type="button" isGoogleSignIn={false} inverted={false}>GO TO CHECKOUT</CustomButton>
-        </div>
-    )
-}
- export default CartDropdown;
+  const cartItems = useSelector((state: any) => state.cart.cartItems);
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((item:any) => (
+          <CartItem key={item.id}
+            price={item.price}
+            imageUrl={item.imageUrl}
+            name={item.name}
+            quantity={item.quantity}
+          ></CartItem>
+        ))}
+      </div>
+      <CustomButton type="button" isGoogleSignIn={false} inverted={false}>
+        GO TO CHECKOUT
+      </CustomButton>
+    </div>
+  );
+};
+export default CartDropdown;
